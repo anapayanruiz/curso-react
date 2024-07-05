26c25ae8569c754e1e5d1ba11ab9de15f0f11118
Ana Payan <ana.payan@sctech.io>
2024-07-05 11:43:26 +0200

    Add mouse follower project

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,7 +1,71 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  // pointer move
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    
+    console.log('effect', enabled);
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // Clean up:
+    // --> cuando el componente se desmonta
+    // --> cuando cambian las dependencias, antes de ejecutar el efecto
+    return () => { // clean up
+      console.log('Clean up');
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  // [] --> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] --> se ejecuta cuando se monta el componente y cada vez que cambia enabled
+  // undefined --> se ejecuta cada vez que se renderiza el componente
+
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', !enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#89f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'}
+      </button>
+    </>
+  )
+}
+
 
 function App() {
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
